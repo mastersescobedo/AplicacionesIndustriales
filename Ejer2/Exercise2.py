f5c20ec4130de07dc52155202b300a805967daea
sergio <s.escobedo.2017@alumnos.urjc.es>
2017-11-24 17:05:01 +0100

    Add coments
    Signed-off-by: sergio <s.escobedo.2017@alumnos.urjc.es>

--- Ejer2/Exercise2.py
+++ Ejer2/Exercise2.py
@@ -2,128 +2,117 @@ import pandas
 import numpy as np
 import cv2
 import math
-from matplotlib import pyplot as plt
+
+# General variable
+number_k_train = 4
+number_k_test = 8
 
 ###################
+# Training
 # K-menas right
 ###################
-for i in range(0,9):
-    data = pandas.read_csv('Train/PasilloDerecha'+str(i)+'.txt', sep=',', header=None, usecols=[2,3], engine='python')
+# Load right data. Iterate over the folder and files with "PasilloDerecha"
+# Store all points from all example in one variable
+for i in range(0, 9):
+    data = pandas.read_csv('Train/PasilloDerecha'+str(i)+'.txt', sep=',', header=None, usecols=[2, 3], engine='python')
     data = data.astype('float32')
-    if i==0:
+    if i == 0:
         ZR = data
     else:
-        ZR = np.vstack((ZR,data))
-#print(data)
+        ZR = np.vstack((ZR, data))
+
 
 # define criteria and apply kmeans()
 criteriaR = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-retR,labelR,centerR=cv2.kmeans(ZR,4,None,criteriaR,10,cv2.KMEANS_RANDOM_CENTERS)
-# Now separate the data, Note the flatten()
-AR = ZR[labelR.ravel()==0]
-BR = ZR[labelR.ravel()==1]
-CR = ZR[labelR.ravel()==2]
-DR = ZR[labelR.ravel()==3]
+# The main output are the CenterR, there is the number_k_points centers form right data
+retR, labelR, centerR = cv2.kmeans(ZR, number_k_train, None, criteriaR, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# # Plot the data
-# plt.scatter(AR[:,0],AR[:,1])
-# plt.scatter(BR[:,0],BR[:,1],c = 'r')
-# plt.scatter(CR[:,0],CR[:,1],c = 'g')
-# plt.scatter(DR[:,0],DR[:,1],c = 'y')
-#
-# plt.scatter(centerR[:,0],centerR[:,1],s = 80,c = 'b', marker = 's')
-# plt.xlabel('Height'),plt.ylabel('Weight')
-# plt.show()
 
 ######################
+# Training
 # K-menas left
 ######################
-for i in range(0,9):
-    data = pandas.read_csv('Train/PasilloIzquierda'+str(i)+'.txt', sep=',', header=None, usecols=[2,3], engine='python')
+# Load left data. Iterate over the folder and files with "PasilloIzquierda"
+# Store all points from all example in one variable
+
+for i in range(0, 9):
+    data = pandas.read_csv('Train/PasilloIzquierda'+str(i)+'.txt', sep=',', header=None, usecols=[2, 3], engine='python')
     data = data.astype('float32')
-    if i==0:
+    if i == 0:
         ZL = data
     else:
-        ZL = np.vstack((ZL,data))
-#print(data)
+        ZL = np.vstack((ZL, data))
 
 # define criteria and apply kmeans()
 criteriaL = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-retL,labelL,centerL=cv2.kmeans(ZL,4,None,criteriaL,10,cv2.KMEANS_RANDOM_CENTERS)
-# Now separate the data, Note the flatten()
-AL = ZL[labelL.ravel()==0]
-BL = ZL[labelL.ravel()==1]
-CL = ZL[labelL.ravel()==2]
-DL = ZL[labelL.ravel()==3]
-#
-# # Plot the data
-# plt.scatter(AL[:,0],AL[:,1])
-# plt.scatter(BL[:,0],BL[:,1],c = 'r')
-# plt.scatter(CL[:,0],CL[:,1],c = 'g')
-# plt.scatter(DL[:,0],DL[:,1],c = 'y')
-#
-# plt.scatter(centerL[:,0],centerL[:,1],s = 80,c = 'b', marker = 's')
-# plt.xlabel('Height'),plt.ylabel('Weight')
-# plt.show()
+retL, labelL, centerL = cv2.kmeans(ZL, number_k_train, None, criteriaL, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 
 ####################
-#Walk
+# Test
 ####################
 
-dataW = pandas.read_csv('Test/Paseante4.txt', sep=',', header=None, usecols=[2,3], engine='python')
-dataW = dataW.astype('float32')
-dataW = np.vstack((dataW,dataW))
+#Load an example to test
+dataT = pandas.read_csv('Test/Paseante4.txt', sep=',', header=None, usecols=[2, 3], engine='python')
+dataT = dataT.astype('float32')
+dataT = np.vstack((dataT,dataT))
 
 
 # define criteria and apply kmeans()
-criteriaW = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-retW,labelW,centerW=cv2.kmeans(dataW,8,None,criteriaW,10,cv2.KMEANS_RANDOM_CENTERS)
-# Now separate the data, Note the flatten()
-# AW = ZR[labelW.ravel()==0]
-# BW = ZR[labelW.ravel()==1]
-# CW = ZR[labelW.ravel()==2]
-# DW = ZR[labelW.ravel()==3]
+criteriaT = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+retT, labelT, centerT = cv2.kmeans(dataT, number_k_test, None, criteriaT, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# # Plot the data
-# plt.scatter(AR[:,0],AR[:,1])
-# plt.scatter(BR[:,0],BR[:,1],c = 'r')
-# plt.scatter(CR[:,0],CR[:,1],c = 'g')
-# #plt.scatter(D[:,0],D[:,1],c = 'y')
-#
-# plt.scatter(centerW[:,0],centerW[:,1],s = 80,c = 'b', marker = 's')
-# plt.xlabel('Height'),plt.ylabel('Weight')
-# plt.show()
+# To decide if somebody is going to right side, move value will get 1 as value or as nearest as possible. If it's to the
+# left, move variable should value 0 or as nearest as possible.
 
-# RIGHT = 0
-# LEFT = 1
+# This variable will represent the direccion
 move = 0
 
-for i in range(0,centerW.shape[0]):
+# Iterate over the number_k_test points detected.
+for i in range(0,centerT.shape[0]):
+    # Reset the nearest distance between points in test and train right
     nearestR = 99
+    # Iterate over number_k_train on the right side
     for t in range(0,centerR.shape[0]):
-        distR = math.sqrt((centerW[i,0]-centerR[t,0]) ** 2 + (centerW[i,1]-centerR[t,1]) ** 2)
+        # Calculate distance between points, test and train
+        distR = math.sqrt((centerT[i,0]-centerR[t,0]) ** 2 + (centerT[i,1]-centerR[t,1]) ** 2)
+        # Get the nearest distance
         if distR<nearestR:
             nearestR=distR
 
+    # Reset the nearest distance between points in test and train left
     nearestL = 99
+    # Iterate over number_k_train on the left side
     for m in range(0, centerR.shape[0]):
-        distL = math.sqrt((centerW[i, 0] - centerL[m, 0]) ** 2 + (centerW[i, 1] - centerL[m, 1]) ** 2)
+        # Calculate distance between points, test and train
+        distL = math.sqrt((centerT[i, 0] - centerL[m, 0]) ** 2 + (centerT[i, 1] - centerL[m, 1]) ** 2)
+        # Get the nearest distance
         if distL < nearestL:
             nearestL = distL
+    # Print the nearest distances on right and left side
+    # print('R=', nearestR,'L=', nearestL)
 
-    print('R=',nearestR,'L=',nearestL)
-    if nearestL>nearestR:
-        move = move+(1/centerW.shape[0])
-        #Sin lo de debajo, solo falla en izquierda12 y derecha 11 limites en 0.8 y 0.2 respectivamente
-    if (nearestL<(nearestR*1.2) and nearestL>nearestR):
-        move= move - (1/(2*centerW.shape[0]))
-    if (nearestR<(nearestL*1.2) and nearestR>nearestL):
-        move = move + (1 / (2 * centerW.shape[0]))
+    # If test point is nearer to right side than left side, add probability to right side
+    if nearestL > nearestR:
+        move = move+(1/centerT.shape[0])
 
-if move>=0.75:
-    print('To the right side',move)
-elif move<=0.25:
-    print('To the left side',move)
+    # Next two ifs are implemented to correct if right side and left side are similar. In this case the probability
+    # added is just the middle so, it stays it in the middle
+    # If it is nearer to right but not too much, remove some probability
+    if nearestR*1.2 > nearestL > nearestR:
+        move = move - (1/(2*centerT.shape[0]))
+
+    # If test point is nearer to left but not too much, add some probability
+    if nearestL*1.2 > nearestR > nearestL:
+        move = move + (1 / (2 * centerT.shape[0]))
+
+# Once each number_k_test points from sample are calculated, the prediction is done
+# Because get an one or a zero is too strict, there are set a margin of 25%
+# This line prints the move's probability
+# print(move)
+if move >= 0.75:
+    print('To the right side')
+elif move <= 0.25:
+    print('To the left side')
 else:
-    print('WARNING, CALL POLICE',move)
+    print('WARNING, CALL POLICE. SOMEBODY IS HANGING AROUND')
